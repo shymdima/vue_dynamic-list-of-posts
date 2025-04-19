4fafa97940d87b9e93f80ee9a92efd5667ade921
dima <shymdima74@gmail.com>
2025-04-19 15:40:24 +0300

    add task solution

--- src/api/users.js
+++
@@ -1,9 +0,0 @@
-import { client } from "./api";
-
-export const getUser = (email) => {
-  return client.get(`/users?email=${email}`);
-};
-
-export const addUser = ({ name, username, email, phone }) => {
-  return client.post("/users", { name, username, email, phone });
-};
